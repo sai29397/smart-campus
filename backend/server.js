0325d1b04c596cf3e3c2b19fcd21b776b3849f21
sai29397 <sai29397@users.noreply.github.com>
2026-09-02 06:34:37 +0530

    perf: non-blocking connection initialization for ultra-fast static and API delivery

--- backend/server.js
+++ backend/server.js
@@ -32,13 +32,7 @@ app.use(
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Asynchronous background DB check (non-blocking)
-app.use((req, res, next) => {
-  if (mongoose.connection.readyState !== 1) {
-    connectDB().catch(() => {});
-  }
-  next();
-});
+
 
 // Request Logger
 app.use((req, res, next) => {
